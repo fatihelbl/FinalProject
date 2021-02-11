a916cd066faf0f601330d43270eb51e8a040f10a
fatihelbl <68300579+fatihelbl@users.noreply.github.com>
2021-02-11 15:51:29 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstact;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            return new Result(true,"Ürün eklendi");
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
+        }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        private bool v1;
+        private string v2;
+
+        public Result(bool succes, string message):this(succes)
+        {
+            Message = message;
+          
+           // ****
+
+        }
+        public Result(bool succes )
+        {
+           
+            Succes = succes;
+        }
+
+        public bool Succes { get; }
+
+        public string Message { get; }
+    }
+}
